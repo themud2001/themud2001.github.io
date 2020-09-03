14932e087882dbd139155c52e0c92fedc4782ffc
THEMUD <themud2001@hotmail.com>
2020-09-03 21:26:38 +0200

    Initial commit

---
+++ precache-manifest.296d8c1d45a2970430e7766f64556464.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51fe69195c236bf590e376e8e17e8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f3422c3ef4fe7bb468f",
+    "url": "/static/css/main.8393d7eb.chunk.css"
+  },
+  {
+    "revision": "4721ef65b2e892ba2c4e",
+    "url": "/static/js/2.8183b58b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8183b58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3422c3ef4fe7bb468f",
+    "url": "/static/js/main.684368da.chunk.js"
+  },
+  {
+    "revision": "7755b94a8d9d9bbcab4a",
+    "url": "/static/js/runtime-main.5ce5820e.js"
+  },
+  {
+    "revision": "22dd36f616c2ece8a4b1cc2217095003",
+    "url": "/static/media/SIMPLIFICA.22dd36f6.ttf"
+  },
+  {
+    "revision": "05d6f36df0f2434581d3f99678324164",
+    "url": "/static/media/roses_background.05d6f36d.jpg"
+  }
+]);
